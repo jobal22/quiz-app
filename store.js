291ccf1666684c797bf1a7485efbc5005d29118d
Jobal <jbaldwije@gmail.com>
2020-02-18 00:24:15 -0600

    working app 1

---
+++ store.js
@@ -0,0 +1,93 @@
+const STORE = {
+    questions: [//1
+    {
+        question: "Why should a Christ-follower share the Gospel?",
+        options: [
+            "Jesus commands every Christ-follower to share the Gospel (see ref Matthew 28:18-20).",
+            "Christ-followers desires for others to know Jesus (see ref 1 Timothy 2:1-4).",
+            "Christ-followers cannot help but to tell others the Gospel (see ref Acts 4:19-20).",
+            "All of the above choices.",
+        ],
+        answer: "All of the above choices."
+    },
+    //2
+    {
+        question: "What is the Gospel?",
+        options: [
+            "The Gospel is the good news about Buddha.",
+            "The Gospel is the good news about Jesus Christ.",
+            "The Gospel is the good news about Muhammad.",
+            "The Gospel is the good news about America.",
+        ],
+        answer: "The Gospel is the good news about Jesus Christ."
+    },
+    //3
+    {
+        question: "What are three major components of the Gospel?",
+        options: [
+            "1) sin 2) solution 3) invitation",
+            "1) sin 2) reformation 3) invitation",
+            "1) money 2) world-peace 3) prosperity",
+            "1) social justice 2) community outreach 3) disaster relief",
+        ],
+        answer: "1) sin 2) solution 3) invitation"
+    },
+    //4
+    {
+        question: "What does the Bible say about humans and sin?",
+        options: [
+            "All have sinned and fall short of the glory of God (ref Romans 3:23).",
+            "Without Jesus Christ, all people are dead in their trespasses and sins. (ref Ephesians 2:1).",
+            "The wages of sin is death (ref Rom 6:23).",
+            "All the above choices.",
+        ],
+        answer: "All the above choices."
+    },
+    //5
+    {
+        question: "What is the “solution” component of the Gospel? (ref 1 Corinthians 15:3-4)?",
+        options: [
+            "Jesus died for our sins in accordance with the Scriptures.",
+            "Jesus was buried, that he was raised on the third day in accordance with the Scriptures.",
+            "Both A and B",
+            "Neither A or B",
+        ],
+        answer: "Both A and B"
+    },
+    //6
+    {
+        question: "Why did Jesus Christ die on the cross and on the third day raise for the grave?",
+        options: [
+            "Jesus died for our sins (see ref 1 Corinthians 5:3).",
+            "Jesus died and rose from the grave so that people can have a relationship with God (see ref John 1:9-13).",
+            "Both A and B",
+            "Neither A or B",
+        ],
+        answer: "Both A and B"
+    },
+    //7
+    {
+        question: "What is the “invitation” component of the Gospel? (see ref Mk 1:15; John 3:16; Romans 10:9-10, 13)",
+        options: [
+            "Every person needs to believe in Jesus and be baptized to be saved?",
+            "You must be born into a Christian home in order to be a Christian?",
+            "Every person must turn from their sin, believe in Jesus, and be baptized to be saved?",
+            "Every person must turn from their sins, believe in Jesus as Savior, and confess Him as Lord.",
+        ],
+        answer: "Every person must turn from their sins, believe in Jesus as Savior, and confess Him as Lord."
+    },
+    //8
+    {
+        question: "Who needs to hear the Gospel?",
+        options: [
+            "All people",
+            "Some people",
+            "No people",
+            "Most people",
+        ],
+        answer: "All people"
+    }
+],
+currentQuestion: 0,
+score: 0
+};
